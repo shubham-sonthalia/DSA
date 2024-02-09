869b506494be0ab255d6091ebffb189c75582e68
Shubham Sonthalia <shubham18366@iiitd.ac.in>
2024-02-09 09:41:11 +0530

    Create BottomViewOfTree.py

---
+++ Trees/BottomViewOfTree.py
@@ -0,0 +1,22 @@
+class Solution:
+    class NodeWithIndex:
+        def __init__(self, node, idx):
+            self.node = node
+            self.idx = idx 
+    def bottomView(self, root):
+        d = {} 
+        queue = [] 
+        queue.append(self.NodeWithIndex(root, 0))
+        while len(queue) != 0: 
+            element = queue.pop(0)
+            d[element.idx] = element.node.data;
+            if element.node.left is not None:
+                queue.append(self.NodeWithIndex(element.node.left, element.idx - 1))
+            if element.node.right is not None:
+                queue.append(self.NodeWithIndex(element.node.right, element.idx + 1))
+        l = list(d.items())
+        l.sort(key = lambda x: x[0])
+        ans = [] 
+        for a, b in l:
+            ans.append(b)
+        return ans

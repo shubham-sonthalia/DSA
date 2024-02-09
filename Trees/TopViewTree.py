d50ac30dd0a2e6fbcfa44b320568d1dddfab5ed4
Shubham Sonthalia <shubham18366@iiitd.ac.in>
2024-02-09 09:44:47 +0530

    Create TopViewTree.py

---
+++ Trees/TopViewTree.py
@@ -0,0 +1,23 @@
+class Solution:
+    class NodeWithIndex:
+        def __init__(self, node, idx):
+            self.node = node
+            self.idx = idx 
+    def topView(self, root):
+        d = {} 
+        queue = [] 
+        queue.append(self.NodeWithIndex(root, 0))
+        while queue: 
+            element = queue.pop(0)
+            if element.idx not in d:
+                d[element.idx] = element.node.data;
+            if element.node.left is not None:
+                queue.append(self.NodeWithIndex(element.node.left, element.idx - 1))
+            if element.node.right is not None:
+                queue.append(self.NodeWithIndex(element.node.right, element.idx + 1))
+        l = list(d.items())
+        l.sort(key = lambda x: x[0])
+        ans = [] 
+        for a, b in l:
+            ans.append(b)
+        return ans

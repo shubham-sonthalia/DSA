ac153dc4935dbc627f548f1b239ff0c627fe4c17
Shubham Sonthalia <shubham18366@iiitd.ac.in>
2024-02-17 10:00:25 +0530

    Create LCAInBST.py

---
+++ BinarySearchTree/LCAInBST.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root is None or root == p or root == q:
+            return root
+        if p.val > root.val and q.val < root.val:
+            return root
+        if p.val < root.val and q.val > root.val:
+            return root
+        if p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        if p.val >root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        return root

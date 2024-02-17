21b7b3b0f464add8f0f5948f552cb7a9b4456c84
Shubham Sonthalia <shubham18366@iiitd.ac.in>
2024-02-17 08:52:37 +0530

    Create searchElementinBST.py

---
+++ BinarySearchTree/searchElementinBST.py
@@ -0,0 +1,9 @@
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if root is None: 
+            return None
+        if root.val == val:
+            return root
+        if root.val < val:
+            return self.searchBST(root.right, val)
+        return self.searchBST(root.left, val)

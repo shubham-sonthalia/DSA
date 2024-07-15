0cc7f9b98f1330022213318c5bfbbd8ea6235039
Shubham Sonthalia <54079322+shubham-sonthalia@users.noreply.github.com>
2024-07-15 09:12:46 +0530

    Update SetMatrixZeros.py

--- Arrays/SetMatrixZeros.py
+++ Arrays/SetMatrixZeros.py
@@ -33,4 +33,46 @@ class Solution:
             for i in range(len(matrix)):
                 matrix[i][0] = 0
 
+
+
+# another attempt 
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        isFirstRowZero = False
+        isFirstColZero = False
+        n = len(matrix)
+        m = len(matrix[0]) 
+        for i in range(n):
+            if matrix[i][0] == 0:
+                isFirstColZero = True
+        for j in range(m):
+            if matrix[0][j] == 0:
+                isFirstRowZero = True
+        for i in range(1,n):
+            for j in range(1,m):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+        for i in range(1,n):
+            if matrix[i][0] == 0:
+                for j in range(m):
+                    matrix[i][j] = 0
+        for j in range(1,m):
+            if matrix[0][j] == 0:
+                for i in range(n):
+                    matrix[i][j] = 0
+        
+        if isFirstRowZero:
+            for j in range(m):
+                matrix[0][j] = 0
+        
+        if isFirstColZero:
+            for i in range(n):
+                matrix[i][0] = 0
+
+
+        
+        
+
         

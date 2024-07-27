080e15345a7a972f6f5dca0fb136b0038f662ec3
Shubham Sonthalia <shubham18366@iiitd.ac.in>
2024-07-27 09:45:06 +0530

    Create MaximumSubarray.cpp

---
+++ Blind75/MaximumSubarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = nums[0];
+        int curSum = 0; 
+        for(int i = 0; i < nums.size(); i++) {
+            if (curSum < 0){
+                curSum = 0;
+            }
+            curSum += nums[i];
+            maxSum = max(curSum, maxSum);
+        }
+        return maxSum;
+    }
+};
